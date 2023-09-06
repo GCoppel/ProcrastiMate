91f9b387dcf8a248a748809f99bfb967207f1bb2
George Coppel <georgelcoppe@gmail.com>
2023-09-06 15:48:50 -0500

    Added a random motivational message to the study page. Also created a nonfunctional button that will, in the future, start the timer when the user beging their study session.

--- src/screens/Study.js
+++ src/screens/Study.js
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const Study = () => {
-    const [isChecked, setChecked] = useState(false);
+  const [isChecked, setChecked] = useState(false);
 
-    const StudyFunc = () => {
-        console.log("Studying")
-    }
+  const StudyFunc = () => {
+    console.log("Studying");
+  };
 
+  const motivationalMessages = [
+    "You're doing great, sweetie!",
+    "Keep up the good work!",
+    "Proud of you, son.",
+  ]
+
+  let motivationalMessage = motivationalMessages[Math.floor(Math.random()*motivationalMessages.length)];
+  
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={StudyFunc} style={styles.signOutBtn}>
-        <Text style={styles.signOutText}>Study</Text>
+      <Text style={styles.motivationalMessageText}>{motivationalMessage}</Text>
+      <TouchableOpacity onPress={StudyFunc} style={styles.studyButton}>
+        <Text style={styles.studyButtonText}>GO</Text>
       </TouchableOpacity>
     </View>
   );
@@ -22,6 +31,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  motivationalMessageText: {
+    fontSize: 25,
+    flexWrap: 'wrap',
+    width: '75%',
+    textAlign: 'center'
+  },
+  studyButton: {
+    marginTop: 20,
+    width: 150,
+    height: 150,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: 'black',
+    backgroundColor: "lightgreen",
+  },
+  studyButtonText: {
+    fontSize: 35,
+    fontWeight: "bold",
+    flexWrap: 'wrap',
+    textAlign: 'center',
   },
 });
 
